eadb0286fafda7ba358c0e226dd211be0b47df42
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-07-21 23:44:27 +0600

    added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://study-sphere-fb1d4.web.app',
+  origin: 'http://localhost:5174',
   credentials: true
 }));
 
@@ -99,7 +99,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const usersCollection = client.db("studysphere").collection("users");
     const sessionsCollection = client.db("studysphere").collection("sessions");
@@ -1582,9 +1582,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
